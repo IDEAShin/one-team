a7b8ed6bc51db38adf86c5b63a9bed512019b018
ideashin <isxin.wong@foxmail.com>
2020-02-07 14:51:18 +0800

    添加zuul

---
+++ one-team-zuul/src/test/java/com/ideashin/one/team/zuul/OneTeamZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideashin.one.team.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTeamZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
