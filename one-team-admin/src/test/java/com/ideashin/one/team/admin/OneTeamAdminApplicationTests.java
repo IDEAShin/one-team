fedd5c6bb4cbc0b47e9c5c1061c9d18fcca1b822
ideashin <isxin.wong@foxmail.com>
2020-02-09 14:00:50 +0800

    增加spring-cloud-admin

---
+++ one-team-admin/src/test/java/com/ideashin/one/team/admin/OneTeamAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideashin.one.team.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTeamAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
